63ea6c867cb2c3aa33496e0789bb3ea3f28dca34
Adith <adithsundram@gmail.com>
2018-12-20 19:39:05 -0500

    test vuf functions

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/testvufposmove.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.Control.AutonomousControl;
+import org.firstinspires.ftc.teamcode.Control.Rover;
+
+@Autonomous(name="testvuf", group ="Smart")
+public class testvufposmove extends AutonomousControl {
+    private ElapsedTime runtime = new ElapsedTime();
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        setup(runtime, Rover.setupType.drive, Rover.setupType.latching, Rover.setupType.vuforia);
+
+        while (opModeIsActive()) {
+            double[] x = {24,12,10};
+rob.vufmovetest(new Rover.Position(x,90));
+
+        }
+    }
+}
